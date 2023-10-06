69a409d9311058ecfbee06e080f474c2322d902b
Billie Thompson <billie.thompson@armakuni.com>
2023-10-06 16:50:36 +0200

    refactor: Formatting for terraform docs resources

--- generate-terraform-docs/example/main.tf
+++ generate-terraform-docs/example/main.tf
@@ -1,8 +1,8 @@
-variable bucket_name {
+variable "bucket_name" {
   type        = string
   description = "The name of the bucket to create"
 }
 
-resource aws_s3_bucket bucket {
+resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
 }
